b79500fa45a73fe9ce29b20fcd7f7a6b5ceb5778
rahulgoyal911 <rahulgoyal0.rg@gmail.com>
2019-12-27 20:26:03 +0530

    list rendering

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -1,16 +1,34 @@
-// rendering a list
+// rendering name list
+// used with person.js
 import React from 'react'
+import Person from './Person'
 
 function NameList() {
-    const names = ['rahul','ankit','prince']
-    return (
-        
-        <div>
-            {
-                names.map(name => <h1>{name}</h1>)
-            }
-        </div>
-    )
+  const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
+  const persons = [
+    {
+      id: 1,
+      name: 'Bruce',
+      age: 30,
+      skill: 'React'
+    },
+    {
+      id: 2,
+      name: 'Clark',
+      age: 25,
+      skill: 'Angular'
+    },
+    {
+      id: 3,
+      name: 'Diana',
+      age: 28,
+      skill: 'Vue'
+    }
+  ]
+  // key can be any unique element corresponding to every object
+  const personList = persons.map(person => <Person key={person.id} person={person} />)
+  const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
+  return <div>{personList}</div>
 }
 
 export default NameList
---
+++ hello-world/src/components/Person.js
@@ -0,0 +1,15 @@
+// rendering one element of a list
+// used with NameList.js
+import React from 'react'
+
+function Person({person, key}) {
+  return (
+    <div>
+      <h2>
+        I am {person.name}. I am {person.age} years old. I know {person.skill}
+      </h2>
+    </div>
+  )
+}
+
+export default Person
